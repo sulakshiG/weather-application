caecbe80d3fac62e8996abd936d7afa243d2934f
sulakshiG <smg.2000gunarathna@gmail.com>
2024-06-28 14:18:09 +0530

    modified environment variable related files

--- weather-application-2/react-weather-application/src/utils/APIHelper.js
+++ weather-application-2/react-weather-application/src/utils/APIHelper.js
@@ -1,5 +1,5 @@
 
 const GET_API_Method = 'https://api.openweathermap.org/data/2.5/group';
-const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY || '802cde8750fb66624216ca766e240cc7';
 
 export const API_URL_withoutCityIds = `${GET_API_Method}?units=metric&appid=${apiKey}`;
